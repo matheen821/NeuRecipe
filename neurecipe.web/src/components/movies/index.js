4eeb10d02873e08c220af77a6700088392ff2d4c
Matheen Shaik <matheen821@gmail.com>
2020-03-31 21:33:11 +0530

    decreased height

--- neurecipe.web/src/components/movies/index.js
+++ neurecipe.web/src/components/movies/index.js
@@ -7,7 +7,7 @@ class Movies extends Component {
       <Iframe
         url="http://localhost:3001/"
         width="100%"
-        height="470vh"
+        height="460vh"
         display="initial"
         position="relative"
       />
